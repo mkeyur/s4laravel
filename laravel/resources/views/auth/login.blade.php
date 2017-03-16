c97a95149d61e91cfc7c850b3bfc4a44a584ae06
root <root@b6b4cb6241ef>
2017-03-16 20:23:21 +0000

    day 3

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -1,68 +1,70 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+@include('layouts.app')
+<script src="{{asset("/plugins/jquery-validation/js/jquery.validate.min.js")}}" type="text/javascript"></script>
+<script src="{{asset("/plugins/jquery-validation/js/additional-methods.min.js")}}" type="text/javascript"></script>
+<div class="row">
+    <div class="portlet-body">
+        <form method="post" class="form-horizontal" id="login" action="{{url('user_login')}}">
+            {{ csrf_field() }}
+            <div class="form-body">
+                <div style="width:40%;margin-bottom: 20px; text-align: center;">
+                @if(Session::has('message'))
+                {{Session::get('message')}}
+                @endif
+              </div>
+                <div class="form-group">
+                    <label class="control-label col-md-3">Email
+                        <span class="required"> * </span>
+                    </label>
+                    <div class="col-md-4">
+                        <input name="email"  value="{{old('email')}}" type="text" class="form-control" />
+                    </div>
                 </div>
+                <div class="form-group">
+                    <label class="control-label col-md-3">Password
+                        <span class="required"> * </span>
+                    </label>
+                    <div class="col-md-4">
+                        <input name="password"  type="password" id="password" class="form-control" />
+                    </div>
+                </div>
+                <div class="form-actions">
+                    <div class="row">
+                        <div class="col-md-offset-3 col-md-9">
+                            <button type="submit" name="cmdSubmit" value="submit" class="btn green">Submit</button>
+                            <a href="{{url('/home')}}" class="btn grey-salsa btn-outline">Cancel</a>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-        </div>
+        </form> 
+
     </div>
 </div>
-@endsection
+<script>
+     $(document).ready(function(){
+         $('#login').validate({
+                errorElement: 'span', //default input error message container
+                errorClass: 'help-block help-block-error', // default input error message class
+                focusInvalid: false, // do not focus the last invalid input
+                ignore: "", // validate all fields including form hidden input
+             rules:{
+                 email:{
+                      required:true,
+                      email:true
+                 },
+                 password:{
+                     required:true         
+                 }
+             },
+             messages:{
+                 email:{
+                     required:'This Field is required'
+                 },
+                 password:{
+                     required:'This field is required'
+                 },
+             }
+         });
+     });
+</script>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,7 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home','HomeController@index');
+Route::auth();
 
-Auth::routes();
+Route::get('candidate/register','Auth\RegisterController@candidate_register');
+Route::post('candidate/sign_up','Auth\RegisterController@candidate_insert_data');
+Route::get('candidate_register/verify/{token}','Auth\RegisterController@confirm_registration_candidate');
 
-Route::get('/home', 'HomeController@index');
+Route::get('employer/register','Auth\RegisterController@employer_register');
+Route::post('employer/sign_up','Auth\RegisterController@employer_insert_data');
+Route::get('employer_register/verify/{token}','Auth\RegisterController@confirm_registration_employer');
+
+Route::get('view_login','Auth\RegisterController@view_login');
+Route::post('user_login','Auth\RegisterController@user_login');
